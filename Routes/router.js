915df59a6b2bd752f21e4e69813b1dbc8e04d91a
Metalli01 <kbkannan2001@gmail.com>
2023-12-21 08:36:25 +0530

    wishlist

--- Routes/router.js
+++ Routes/router.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const router= new express.Router()
 const productController = require('../Controllers/productController')
+const userController = require('../Controllers/userController')
+const wishlistController = require('../Controllers/wishlistController')
 
+const jwtMiddleware = require('../Middlewares/jwtMiddleware')
 // get all products
 router.get('/products/all',productController.getAllProductsController)
 
+// for register user
+router.post('/user/register',userController.registerController)
+
+// for login
+router.post('/user/login',userController.loginController)
+
+// for product details
+router.get('/product/get/:id',productController.getProductController)
+
+//add to wishlist
+router.get('/wishlist/add/:id',jwtMiddleware,wishlistController.addToWishlistController)
 
 module.exports =router
